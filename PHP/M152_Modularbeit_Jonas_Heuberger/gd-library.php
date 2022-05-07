7908a979a41cdfccd34af064d5da27b67b271223
Jonas_Heuberger <90388219+Jonas-Heuberger@users.noreply.github.com>
2022-05-07 17:25:14 +0200

    I started my Modulproject

---
+++ PHP/M152_Modularbeit_Jonas_Heuberger/gd-library.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>GD Library</title>
+    <link rel="stylesheet" href="CSS/style.css">
+</head>
+<body>
+    <div class="Picture">
+    <?php
+// Ein bestehendes Bild nehmen
+$img1 = imagecreatefromjpeg('Multimedia/Pictures/Döner.jpeg');
+$img2 = imagecreatefromjpeg('Multimedia/Pictures/Logo.jpeg');
+
+$img1 = add_text($img1);
+picture($img1, $img2);
+//Funktion um Text aufs Bild zu bringen
+function add_text($img)
+{
+    $white = imagecolorallocate($img, 255, 255, 255);
+    $font = "Font/arial.ttf";
+    //    Schriftgrösse, Neigung, x-Koordinate, y-Koordinate, Farbe, Schriftart, Text  
+    imagettftext($img, 50, 0, 830, 315, $white, $font, 'Jonas');
+    imagettftext($img, 50, 0, 920, 380, $white, $font, 'I2a'); 
+
+    return $img;
+}
+
+function picture($img1, $img2)
+{
+    // Grössen von img1 bekommen
+    $width = imagesx($img1);
+    $height = imagesy($img1);
+
+    // Die Mitte von img1 ausrechnen
+    $centerX = $width / 2;
+    $centerY = $height / 2;
+
+    // Grössen von img2 bekommen
+    $width = imagesx($img2);
+    $height = imagesy($img2);
+
+    // Die Mitte von img2 ausrechnen
+    $centerX = $centerX - $width / 2;
+    $centerY = $centerY - $height / 2;
+
+    //Beide Bilder zusammenfügen
+    imagecopy($img1, $img2, $centerX, $centerY, 0, 0, 100, 100);
+
+    //Speicherung und Ausgabe
+    imagejpeg($img1, "Multimedia/Pictures/GD_Picture.jpg", 100);
+    echo  '<a href="gd-library.php"><img src="Multimedia/Pictures/GD_Picture.jpg"/></a>';
+}
+?>
+</div>
+</body>
+</html>
+
---
+++ PHP/M152_Modularbeit_Jonas_Heuberger/gif.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php include('Include/header.php'); ?>
+    <img src="Multimedia/GIF/GIF.gif">
+    <div>
+       <p>Untertitel</p> 
+       <p>Grösse: 959 KB</p>
+
+    </div>
+</body>
+</html>
