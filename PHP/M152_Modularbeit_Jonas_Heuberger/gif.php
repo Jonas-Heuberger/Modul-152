52be6ef24c97cf97cfc029758515b938c19e70b7
Jonas_Heuberger <90388219+Jonas-Heuberger@users.noreply.github.com>
2022-05-11 09:29:50 +0200

    almost done

--- PHP/M152_Modularbeit_Jonas_Heuberger/gif.php
+++ PHP/M152_Modularbeit_Jonas_Heuberger/gif.php
@@ -4,10 +4,17 @@
 <body>
     <?php include('Include/navbar.php'); ?>
 
-    <div class="GIF"><img src="Multimedia/GIF/GIF.gif" alt=""></div>
-    <div class="Subtitle"><p>Untertitel</p></div>
-    <div class="FileSize">Grösse: 959 KB</div>
-
+    <div class="GIF">
+    <div class="card">
+    <img src="Multimedia/GIF/GIF.gif" class="card-img-top" alt="...">
+    <div class="card-body">
+        <p class="card-text">
+            Untertitel<br>
+            Grösse: 959 KB
+        </p>
+    </div>
+</div>
+</div>
 <?php include 'Include/footer.php'; ?>
 </body>
 </html>
